51cba72e9af31d86b424d4dab826e364df0609a3
viktor bezuglov <bezza8418@gmail.com>
2026-02-22 17:52:38 +0500

    обновлен пакет __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,8 +1,15 @@
 """Пакет для маскирования банковских реквизитов."""
 
-from src.masks import get_mask_account, get_mask_card_number, mask_financial_info, mask_personal_data
+from src.masks import (
+    get_mask_account,
+    get_mask_card_number,
+    mask_financial_info,
+    mask_personal_data,
+    process_user_data,
+)
 from src.widget import get_date, mask_account_card
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
+from src.decorators import log  # Добавляем импорт декоратора
 
 __all__ = [
     # Из mask.py
@@ -10,6 +17,7 @@ __all__ = [
     "get_mask_card_number",
     "mask_personal_data",
     "mask_financial_info",
+    "process_user_data",
     # Из widget.py
     "mask_account_card",
     "get_date",
@@ -17,4 +25,6 @@ __all__ = [
     "filter_by_currency",
     "transaction_descriptions",
     "card_number_generator",
+    # Из decorators.py
+    "log",
 ]
